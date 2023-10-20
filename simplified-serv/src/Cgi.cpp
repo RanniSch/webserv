1b220b2971b5c14a93e3e25e39251a5bb244a1bb
rschlott <rschlott@student.42wolfsburg.de>
2023-10-20 17:51:26 +0200

    cgi

--- simplified-serv/src/Cgi.cpp
+++ simplified-serv/src/Cgi.cpp
@@ -28,10 +28,6 @@ void Cgi::runCgi()
 	ResponseMessage findRequest(cRequest);
 	std::string cgiPath = findRequest.get_cgi_path();
 	//std::cout << "_cgiPath_" << cgiPath << std::endl;
-	//std::string temp = _client.cgiPath; // I need the path of the python script; will be used to execute! Max has not checked if cgi data exists!
-    //const char* out_filename = temp.c_str();
-    //int outfile = open(out_filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-    //if(outfile == -1)
 
 	//create arguments for execve
 	std::string scriptPath = findRequest.get_target_path();
@@ -43,44 +39,16 @@ void Cgi::runCgi()
 	// sting1 = a=b; string2 = c=d ... könnten querys auch auf 6 begrenzen
 	// als env setzen & kleingeshrieben lassen
 
-    /*int pipe_d[2];
-	if (pipe(pipe_d) == -1)
-	{
-		std::cout << "Pipe Error" << std::endl;
-		throw(CgiException());
-	}
-		
-	// std::string header inclusive 2x \r\n\r\n
-	if (_client.header.size() > 0)      // here I need the header string
-		fcntl(pipe_d[WRITE_END], 0, _client.header.size());
-
-	write(pipe_d[WRITE_END], _client.header.c_str(), _client.header.size());
-	close(pipe_d[WRITE_END]);*/
-
     int cgiPid = fork();
 	//if (cgi_pid < 0)
 
     if (cgiPid == 0)
 	{
-		//set standard output to file
-		//dup2(outfile, STDOUT_FILENO);
-		//close(outfile);
-
-		//create arguments for execve
-		//char* scriptPath = (char*)(_client.path_on_server.c_str()); // I need the location in Server; so the path of the Python Script
-
-		//const char* pathToPython3 = "/usr/bin/python3";
 		const char* _args[3];
 		//_args[0] = (char *)pathToPython3;
 		_args[0] = cgiPath.c_str();
 		_args[1] = scriptPath.c_str();
 		_args[2] = NULL;
-
-		//dup2(pipe_d[READ_END], STDIN_FILENO);
-		//close(pipe_d[READ_END]);
-
-		//dup2(outfile, STDOUT_FILENO);
-		//close(outfile);
 
 		char* envVariabe = const_cast<char*>(queryString.c_str());
 		// Sets an environment variable
@@ -95,12 +63,6 @@ void Cgi::runCgi()
 	}
 
 	// Child process gibt response als string an parent zurück und diese muss dann zur ResponseMessage für die Browserausgabe 
-
-    // Sets necessary CGI-Environment variables
-    //setenv("REQUEST_METHOD", _client.getRequestMethod().c_str(), 1); // I need those informations
-    //setenv("QUERY_STRING", _client.getQueryString().c_str(), 1);
-    //setenv("CONTENT_TYPE", _client.getContentType().c_str(), 1);
-    //setenv("CONTENT_LENGTH", _client.getContentLength().c_str(), 1);
 }
 
 
