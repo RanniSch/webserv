21af75c614a2bab32da75545f27605976cadbeee
MaxIhme <maxrehberg@posteo.de>
2023-10-22 21:11:45 +0200

    directory listings class nearly complete. Think of what to do with invisible files ('.DS_Store') and check the links to the files and to the subdir and dir above

--- simplified-serv/src/DirectoryListing.cpp
+++ simplified-serv/src/DirectoryListing.cpp
@@ -10,10 +10,14 @@ std::string	DirectoryListing::create_listing_html( std::string path )
 {
 	std::string out;
 
+	if ( !dir_exists(path) )
+		return "";  // was anderes als Fehler??
+
 	out = "";
 	out += _create_head( path );
 	out += _create_parent_dir();
 	out += _create_all_listings( path );
+	out += _end();
 
 	return out;
 }
@@ -55,23 +59,37 @@ std::string	DirectoryListing::_create_all_listings( std::string dir_path )
 		std::cerr << "Error opening the directory." << std::endl; // was besseres machen
 	while ((entry = readdir(dir)))
 	{
-		out += _create_one_listing( entry );
+		out += _create_one_listing( entry, dir_path );
 	}
 
 	closedir(dir);
 	return out;
 }
 
-std::string	DirectoryListing::_create_one_listing( struct dirent* entry )
+std::string	DirectoryListing::_create_one_listing( struct dirent* entry, std::string path )
 {
 	std::string out;
 	std::string name_of_item;
-	std::string item_last_modified = "last_mod";
+	std::string item_last_modified;
 	std::string item_size = "size__";
+	bool		file_flag = false;
 
 	name_of_item = entry->d_name;
 	if ( name_of_item == "." || name_of_item == "..")
 		return "";
+	path = path_one_plus_path_two(path, name_of_item);
+	if ( dir_exists(path) )
+	{}
+	else if ( file_exists(path) )
+		file_flag = true;
+	else
+		return "";
+	item_last_modified = _last_modified ( path );
+	if ( file_flag )
+		item_size = _size( path );
+	if ( !file_flag )
+		name_of_item += "/";
+
 
 	out = "";
 	out += "<tr><td><a href=\"";
@@ -87,32 +105,63 @@ std::string	DirectoryListing::_create_one_listing( struct dirent* entry )
 	return out;
 }
 
-// #include <iostream>
-// #include <cstdlib>
-// #include <cstring>
-// #include <dirent.h>
+std::string	DirectoryListing::_last_modified( std::string path )
+{
+	struct stat fileStat;
+	struct timespec time;
+	if (stat(path.c_str(), &fileStat) == 0)
+		time = fileStat.st_mtimespec;
+	time_t timeInSeconds = time.tv_sec;
 
-// int main() {
-//     const char* directoryPath = "/path/to/your/directory"; // Replace with the actual directory path
+    // Convert time_t to a tm structure
+    // struct tm* timeinfo = gmtime(&timeInSeconds);
+    struct tm* timeinfo = localtime(&timeInSeconds);
 
-//     DIR* directory = opendir(directoryPath);
-//     if (directory) {
-//         struct dirent* entry;
-//         while ((entry = readdir(directory))) {
-//             if (entry->d_type == DT_REG) {
-//                 // Regular file
-//                 std::cout << "File: " << entry->d_name << std::endl;
-//             } else if (entry->d_type == DT_DIR) {
-//                 // Subdirectory
-//                 if (std::strcmp(entry->d_name, ".") != 0 && std::strcmp(entry->d_name, "..") != 0) {
-//                     std::cout << "Directory: " << entry->d_name << std::endl;
-//                 }
-//             }
-//         }
-//         closedir(directory);
-//     } else {
-//         std::cerr << "Error opening the directory." << std::endl;
-//     }
+    // Format the time as a string (year-month-day hour:minute)
+    char timeString[20];  // Sufficient size to hold the formatted string
+    strftime(timeString, sizeof(timeString), "%Y-%m-%d %H:%M", timeinfo);
 
-//     return 0;
-// }
+	return timeString;
+}
+
+std::string	DirectoryListing::_size( std::string path )
+{
+	struct stat				fileStat;
+	long int				size;
+	long int				rest;
+	std::stringstream		ss;
+	int						i;
+
+// if file if directory
+	if (stat(path.c_str(), &fileStat) != 0)
+		return "";
+	size = (long int)fileStat.st_size;
+	for ( i = 0; i < 3 && size > 1000; i++ )
+	{
+		rest = size % 1000;
+		size /= 1000;
+	}
+	if ( size < 0 )
+		return "";
+	ss << size;
+	if ( size < 10 )
+		ss << "." << rest / 100;
+	switch (i)
+	{
+		case (1):
+			ss << "K";
+			break;
+		case (2):
+			ss << "M";
+			break;
+		case (3):
+			ss << "G";
+			break;
+	}
+	return (ss.str());
+}
+
+std::string	DirectoryListing::_end( void )
+{
+	return ("</tbody></table></body></html>");
+}
