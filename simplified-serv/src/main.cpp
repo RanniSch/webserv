ee30fa59bf6a671c1049101c6e5b6e37f02e45a2
MaxIhme <maxrehberg@posteo.de>
2023-10-31 16:13:10 +0100

    changed a bit in main for the valgrind error 1 again

--- simplified-serv/src/main.cpp
+++ simplified-serv/src/main.cpp
@@ -23,7 +23,10 @@ int main(int argc, char **argv)
 {
 	//std::string												path_config_file;
 	if ( argc != 2)
+	{
 		std::cout << "Error: please give me the config file as argument" << std::endl;
+		exit(1);
+	}
 	(void)argv;
 	Config config(argv[1]);
 	g_config = &config; // raus
