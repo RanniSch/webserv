3eea1049b2e94c735a35ec41de413fecf9a1bd34
rschlott <rschlott@student.42wolfsburg.de>
2023-11-02 10:17:07 +0100

    broken-script

---
+++ simplified-serv/www/cgi-bin/script-broken.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+
+######## Set the content type to HTML
+content_type = "Content-Type: text/html\n"
+
+######### Create header for response
+header = """
+
+<title>CGI POST Output</title> """
+fo:
+if:
+ksjdhf
+
+######### End the HTML document
+footer = ""
+
+######### Combine all parts into the response
+response = header + answer + footer
+
+######### Send the response to the standard output
+print(response)
